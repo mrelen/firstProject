8570b6b0743dbfe75ca1eec2bf5f0c8362026bed
Name <email@example.ru>
2023-02-22 12:38:12 -0400

    fist comit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template> 
+  <div class="bg-gray-800 w-full text-white py-12 px-12">
+    <div class="container">
+        <div class="grid grid-cols-4">
+            <div>12</div>
+            <div>12</div>
+
+            <div>12</div>
+
+            <div>12</div>
+
+        </div>
+    </div>
+  </div>
+    </template>
+    
